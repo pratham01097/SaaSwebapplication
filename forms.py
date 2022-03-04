9a7f1a923d2e9a9fe99ae40ade2704e22474b7b8
pratham01097 <72733710+pratham01097@users.noreply.github.com>
2022-03-04 20:46:44 +0000

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
-class RegisterForm(FlaskForm):
-    username=StringField(label="User Name")
-    email_address=StringField(label="Email Address: ")
-    password1=PasswordField(label="New password")
-    password2=PasswordField(label="confirm your password")
-    submit=SubmitField(label='submit')
